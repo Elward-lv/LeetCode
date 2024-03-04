475a40770abec1d5e37298d52f8c9a4b4cc8b7dc
lvyanwei <lvyanwei@zhaojiafang.com>
2024-03-04 18:21:43 +0800

    110平衡二叉树的判断 自顶向下 自底向上

---
+++ src/repeat2024/leetcode/editor/cn/BalancedBinaryTree.java
@@ -0,0 +1,131 @@
+//给定一个二叉树，判断它是否是高度平衡的二叉树。 
+//
+// 本题中，一棵高度平衡二叉树定义为： 
+//
+// 
+// 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。 
+// 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [1,2,2,3,3,null,null,4,4]
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：true
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中的节点数在范围 [0, 5000] 内 
+// -10⁴ <= Node.val <= 10⁴ 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉树 👍 1487 👎 0
+
+
+package repeat2024.leetcode.editor.cn;
+
+public class BalancedBinaryTree {
+    public static void main(String[] args) {
+        Solution solution = new BalancedBinaryTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+
+        private Boolean res = true;
+       /* public boolean isBalanced(TreeNode root) {
+            //1.递归：平衡树的每个子树都是高度差都是小于等于1的 （自顶向下的计算 每个节点会出现重复计算）
+            if(root == null){
+                return res;
+            }
+            deep(root);
+            return res;
+            //2.学习  自底向上的计算
+
+        }
+
+        private Integer deep(TreeNode root){
+            if(root == null){
+                return 0;
+            }
+            Integer l = deep(root.left);
+            Integer r = deep(root.right);
+            if(Math.abs( l - r) > 1){
+                res = false;
+            }
+            return Math.max(l, r) + 1;
+        }*/
+
+        public boolean isBalanced(TreeNode root) {
+            //2.自底向上递归的做法类似于后序遍历，对于当前遍历到的节点，先递归地判断其左右子树是否平衡，再判断以当前节点为根的子树是否平衡。如果一棵子树是平衡的，则返回其高度（高度一定是非负整数），否则返回 −1-1−1。如果存在一棵子树不平衡，则整个二叉树一定不平衡。
+            return height(root) >= 0;
+        }
+
+        public int height(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            int leftHeight = height(root.left);
+            int rightHeight = height(root.right);
+            if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+                return -1;
+            } else {
+                return Math.max(leftHeight, rightHeight) + 1;
+            }
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
