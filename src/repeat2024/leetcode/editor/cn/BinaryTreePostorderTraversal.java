7536e37576ddb3672d01f0ab95ecbb8bb495f79c
lvyanwei <lvyanwei@zhaojiafang.com>
2024-06-18 15:47:27 +0800

    145非递归未实现 老是代码错误提交问题

--- src/repeat2024/leetcode/editor/cn/BinaryTreePostorderTraversal.java
+++ src/repeat2024/leetcode/editor/cn/BinaryTreePostorderTraversal.java
@@ -41,55 +41,82 @@
 
 package repeat2024.leetcode.editor.cn;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class BinaryTreePostorderTraversal{
+public class BinaryTreePostorderTraversal {
     public static void main(String[] args) {
         Solution solution = new BinaryTreePostorderTraversal().new Solution();
-   }
-    //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        dfs(root, result);
-        return result;
     }
+    //leetcode submit region begin(Prohibit modification and deletion)
 
-    public static void dfs(TreeNode root, List<Integer> result){
-        if(root != null){
-            dfs(root.left, result);
-            dfs(root.right, result);
-            result.add(root.val);
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public List<Integer> postorderTraversal(TreeNode root) {
+            //1.递归实现后序遍历  如果不使用递归的话 需要自己维护栈 因为递归也是使用栈的方式保存对应的信息的 需要用栈记录所有的节点顺序信息
+            List<Integer> result = new ArrayList<>();
+            if (root == null) {
+                return result;
+            }
+            //2.非递归实现
+            Stack<TreeNode> stack = new Stack<>();
+            Set<TreeNode> visitedNodes = new HashSet<>();
+            stack.push(root);
+            visitedNodes.add(root);
+            while (!stack.isEmpty()) {
+                TreeNode node = stack.pop();
+                if (root.left != null && !visitedNodes.contains(root.left)) {
+                    stack.push(root.left);
+                    visitedNodes.add(root.left);
+                }
+                if (root.right != null && !visitedNodes.contains(root.right)) {
+                    stack.push(root.right);
+                    visitedNodes.add(root.right);
+                }
+                result.add(node.val);
+            }
+            return result;
+        }
+
+        public static void dfs(TreeNode root, List<Integer> result) {
+            if (root != null) {
+                dfs(root.left, result);
+                dfs(root.right, result);
+                result.add(root.val);
+            }
+        }
+
+        //leetcode submit region end(Prohibit modification and deletion)
+    }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
         }
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
- public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
 }
